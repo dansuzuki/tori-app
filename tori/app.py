37c83bcef97ebe15c6e86c3bd0baa1dd36d93dee
kuziandus <dqsuzuki@talas.ph>
2017-09-27 19:11:56 +0800

    doing the lambda instead already using virtualenv for minimum requirements.txt

--- tori/app.py
+++ tori/app.py
@@ -1,26 +1,34 @@
-from kafka import KafkaProducer
+import couchdb, itertools, json, tweepy
 
-import couchdb
-import json
-import tweepy
 
 class Listener(tweepy.StreamListener):
-    def __init__(self):
-        #self.producer = KafkaProducer(bootstrap_servers = '127.0.0.1:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-	self.couch = couchdb.Server()
-	self.db = self.couch['tweets']
+
+    def __init__(self, db):
+        self.db = db
+        self.n = 0
+        self.most_recent = 0
+        self.places = []
 
     def on_data(self, data):
         try:
             msg = json.loads( data )
-            screen_name = msg['user']['screen_name'].encode('utf-8')
-            tweet_text = msg['text'].encode('utf-8')
-            print screen_name + ' tweets:'
-            print tweet_text
-            print '------------------'
-            print
 
-            self.db.save(msg)
+            timestamp_ms = int(msg['timestamp_ms'])
+            hash_tags = map(lambda f: f['text'], msg['entities']['hashtags'])
+            screen_name = msg['user']['screen_name'].encode('utf-8')
+            place_full_name = msg['place']['full_name'].encode('utf-8')
+
+            self.places_add(place_full_name, timestamp_ms)
+
+            self.n = self.n + 1
+            if self.n % 10 == 0:
+                self.places_window(3600000)
+
+                for kv in self.places_top_k(10):
+                    print '%s %d' % (kv[0], kv[1])
+
+                print '------------------'
+                print
 
             return True
         except BaseException as e:
@@ -36,7 +44,31 @@ class Listener(tweepy.StreamListener):
         else:
             return True
 
+
+    def places_add(self, place, timestamp_ms):
+        # update most recent
+        if self.most_recent < timestamp_ms:
+            self.most_recent = timestamp_ms
+
+        self.places.append((place, timestamp_ms))
+
+    def places_top_k(self, k):
+        grouped = itertools.groupby(map(lambda kv: (kv[0], 1), self.places), lambda kv: kv[0])
+        for_reduce = map(lambda kv : (kv[0], map(lambda n: n[1], kv[1])), grouped)
+        reduced = map(lambda kv: (kv[0], sum(kv[1])), for_reduce)
+        return sorted(reduced, key = lambda kv: kv[1], reverse = True)[:k]
+
+    def places_window(self, window):
+        earliest = self.most_recent - window
+        self.places = filter(lambda t: t[1] > earliest, self.places)
+
+
+
+
+
 def main():
+
+
     consumer_key = '9XUx4HdhMURjELBO9PT6eWCIA'
     consumer_secret = 'utmsvuQUSdBUkM5axLtIjVtr1CHGMPl5QDxypVUrk7d713LIKd'
 
@@ -46,7 +78,12 @@ def main():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    stream = tweepy.Stream(auth, Listener())
+    #couch = couchdb.Server()
+    #db = couch['tweets']
+
+    listener = Listener(None)
+
+    stream = tweepy.Stream(auth, listener)
     stream.filter(locations = [116.930, 4.650, 126.600, 20.840])
 
 if(__name__ == '__main__'):
